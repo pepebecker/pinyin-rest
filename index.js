1bfc6a92cee372cf78d8b771a27bd6bde6c8dde7
Pepe Becker <mail@pepebecker.com>
2017-04-19 01:58:38 +0200

    Init

---
+++ index.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const convertPinyin = require('pinyin-converter')
+const splitPinyin = require('pinyin-split')
+const findHanzi = require('find-hanzi')
+const express = require('express')
+const app = express()
+
+app.get('/hanzi/:query', function (req, res) {
+	findHanzi(req.params.query, req.query).then((data) => res.send(data), (error) => res.send(error))
+})
+
+app.get('/pinyin/:query', function (req, res) {
+	if (req.query.split) {
+		splitPinyin(req.params.query).then((data) => res.send(data), (error) => res.send(error))
+	} else {
+		convertPinyin(req.params.query, req.query).then((data) => res.send(data), (error) => res.send(error))
+	}
+})
+
+app.listen(3000)
